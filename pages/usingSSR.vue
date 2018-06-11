863689b09d181a85fb4ee1f4ca08728c8a06ee3d
Daniel <danielsalles@gmail.com>
2018-06-11 21:37:47 -0200

    init commit

---
+++ pages/usingSSR.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    result: {{ result }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+  return { result: '' }
+},
+async asyncData ({ $axios }) {
+  const series = await $axios.$get('https://api1.soseriados.net/series?page=1&sort=rating&order=desc&limit=8&nameLIKE=g%')
+  return { result: series }
+}
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ pages/usingnossr.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    result: {{ result }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+  return { result: '' }
+},
+mounted: async function() {
+  const series = await this.$axios.$get('https://api1.soseriados.net/series?page=1&sort=rating&order=desc&limit=8&nameLIKE=g%')
+
+  this.result = series
+}
+}
+</script>
+
+<style lang="css">
+</style>
